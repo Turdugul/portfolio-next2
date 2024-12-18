a4e9a2f1061a476ffc67d2fb508fd68de444d9cd
Turdugul <okonbaevaturdugul@gmail.com>
2024-12-18 10:52:50 +0000

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,8 +63,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I&apos;m Turdugul (Gul).</span>{' '}
-        I&apos;m a{' '}
-        <span className="font-bold"> junior software developer. </span>
+        I&apos;m a <span className="font-bold"> software developer. </span>
         {/*with{' '}
         <span className="font-bold"> years</span> of experience. */}
         I enjoy building <span className="italic">sites & apps</span>. My focus
